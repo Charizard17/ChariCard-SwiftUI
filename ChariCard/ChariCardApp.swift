de6d50b98ec7011c9d9b82380969f16e9bbdaae9
Charizard17 <3723esad@gmail.com>
2023-06-14 15:16:09 +0200

    initial commit

---
+++ ChariCard/ChariCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChariCardApp.swift
+//  ChariCard
+//
+//  Created by Esad Dursun on 14.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ChariCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
